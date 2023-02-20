2b3e9f608c95161db2ba33ab7ef471822ce20702
clint360 <clint360.rebase@gmail.com>
2023-02-20 18:13:03 +0100

    API Fetch

--- src/App/Pages/Favourites.js
+++ src/App/Pages/Favourites.js
@@ -7,59 +7,61 @@ import { Link } from 'react-router-dom';
 
 
 function Favourites() {
-    const { myRecipes, setMyRecipes, setrandchange, currentIndex, setCurrentIndex } = useContext(Context);
-    const [indexer, setIndexer] = useState(0)
-    const [deleteModalPosition, setDeleteModalPosition] = useState('translateY(-400px)')
-  
-    const deleteRecipe = () => {
-       if(myRecipes[indexer]) {
-        myRecipes.splice(indexer, 1);
-        setMyRecipes(myRecipes);
-       }
-       setDeleteModalPosition('translateY(-400px)');
-       console.log(myRecipes)
-       setrandchange('...')
-    }
-    
-    const open = () => {
-      setDeleteModalPosition('translateY(0px)');
-    }
-  
-    const close = () => {
-      setDeleteModalPosition('translateY(-400px)');
-    }
-  
-    const pop = {
-      transform: deleteModalPosition
-    }
-  
-    useEffect(()=>{
-       
+  const { myRecipes, setMyRecipes, setrandchange, currentIndex, setCurrentIndex } = useContext(Context);
+  const [indexer, setIndexer] = useState(0)
+  const [deleteModalPosition, setDeleteModalPosition] = useState('translateY(-400px)')
+
+  const deleteRecipe = () => {
+    if (myRecipes[indexer]) {
+      myRecipes.splice(indexer, 1);
       setMyRecipes(myRecipes);
-    },)
-  
-    return (
-      <div className='recipespage'>
-               <div className='delpop' style={pop}> <Delete yes={deleteRecipe} close={close} recipeIndex={indexer} /> </div>
-         <section id='headersection'>
-          <p>FAVOURITES</p>    
-          </section>
-         <section className='recipecards here'> 
-          {myRecipes.filter(obj => {
-  return obj.favorite === true;
-}).map((items, index) => { return (
-         <RecipeCard name={items.name} 
-         image={items.image} 
-         lm={items.lm}
-         open={()=>{open(); setIndexer(index)}}
-         pic={()=>{setCurrentIndex(index)}}
-         index={myRecipes.indexOf(items)}
-         present={false}
-          />
-           )})}
-         </section>
-      </div>
-    )
+    }
+    setDeleteModalPosition('translateY(-400px)');
+    console.log(myRecipes)
+    setrandchange('...')
+  }
+
+  const open = () => {
+    setDeleteModalPosition('translateY(0px)');
+  }
+
+  const close = () => {
+    setDeleteModalPosition('translateY(-400px)');
+  }
+
+  const pop = {
+    transform: deleteModalPosition
+  }
+
+  useEffect(() => {
+
+    setMyRecipes(myRecipes);
+  },)
+
+  return (
+    <div className='recipespage'>
+      <div className='delpop' style={pop}> <Delete yes={deleteRecipe} close={close} recipeIndex={indexer} /> </div>
+      <section id='headersection'>
+        <p>FAVOURITES</p>
+      </section>
+      <section className='recipecards here'>
+        {myRecipes.filter(obj => {
+          return obj.favorite === true;
+        }).map((items, index) => {
+          return (
+            <RecipeCard name={items.name}
+              image={items.image}
+              lm={items.lm}
+              open={() => { open(); setIndexer(index) }}
+              pic={() => { setCurrentIndex(index) }}
+              index={myRecipes.indexOf(items)}
+              present={false}
+            />
+          )
+        })}
+      </section>
+    </div>
+  )
 }
 
 export default Favourites
